8921de93c371b492d8d8de9a9bad73da155b1045
艾蒙迪 <823624595@qq.com>
2022-05-22 15:43:27 +0800

    Initialization. Entrance and configurations for springboot, and some utility classes.

---
+++ src/main/java/com/ablog/MyBlogApplication.java
@@ -0,0 +1,15 @@
+package com.ablog;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.wip.dao")
+//@EnableCaching
+public class MyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBlogApplication.class, args);
+    }
+}
